dbf57d1d9498484416a433180f3460a31c9b7e4c
Deven Parekh <deven.parekh@mail.mcgill.ca>
2026-07-13 15:15:02 -0400

    Adding Tips activity

---
+++ Tips/src/index.tsx
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import ReactDOM from "react-dom";
+import * as React from "react";
+import { AppContainer } from "react-hot-loader";
+import LearnTips from "./components/LearnTips";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+const eventMethod = "addEventListener";
+const eventer = window[eventMethod];
+const messageEvent = "message";
+
+ eventer(
+  messageEvent, (e: any) => {
+     const rootElement = document.getElementById("root") as HTMLElement;
+
+    if (!!rootElement) {
+       const root = createRoot(rootElement);
+       root.render(<AppContainer>
+         <LearnTips data={e.data} />
+       </AppContainer>);
+     }
+   },
+   false
+ )
+
